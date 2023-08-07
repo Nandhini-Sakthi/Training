b13e78b3cf70b40f7ff2f0b7e99d38b004c801ad
nandhini <nandhini.s.interns@metamation.com>
2023-08-07 15:16:52 +0530

    basic assignment 1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
 ﻿using System;
-Console.WriteLine ("Hello, World!");
-
-   
+using System.Linq.Expressions;
+Console.Write ("Enter a decimal number: ");
+int decimalnum = int.Parse (Console.ReadLine());
+string hexadecimalnum = decimalnum.ToString("X");
+Console.WriteLine ($"Hexadecimal: {hexadecimalnum}");
+string binary = "";
+while (decimalnum > 0) {
+   int remainder = decimalnum % 2;
+   binary= Convert.ToString (remainder) + binary;
+   decimalnum /= 2; }
+ Console.WriteLine ($"Binary:{binary}");
