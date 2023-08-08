dc8abdfd81e781d6183e9861a04283fe7e381116
nandhini <nandhini.s.interns@metamation.com>
2023-08-08 10:02:16 +0530

    basic assignment 3

--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
 ﻿using System;
-Console.WriteLine ("Hello, World!");
-
-   
+int firstNum = 0, SecondNum = 1, nextNum, num;
+Console.Write ("Enter the number: ");
+num = int.Parse (Console.ReadLine ());
+if (num < 2) {
+   Console.Write ("Please Enter a number greater than two");
+} else {
+   Console.Write (firstNum + " " + SecondNum + " ");
+   for (int i = 2; i < num; i++) {
+      nextNum = firstNum + SecondNum;
+      Console.Write (nextNum + " ");
+      firstNum = SecondNum;
+      SecondNum = nextNum;
+   }
+}
