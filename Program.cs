bcfb41f602625da15c9022c186ee8af6a903be91
nandhini <nandhini.s.interns@metamation.com>
2023-08-09 14:40:32 +0530

    basic assignment t06

--- Program.cs
+++ Program.cs
@@ -1,4 +1,16 @@
 ﻿using System;
-Console.WriteLine ("Hello, World!");
+Console.Write ("Enter a word or phrase:");
+string text = Console.ReadLine ();
+text = text.Replace (" ", "").ToLower ();
+char[] ch = text.ToCharArray ();
+Array.Reverse (ch);
+string rev = new string (ch);
+bool b = text.Equals (rev); 
+if (b == true) {
+   Console.WriteLine ("" + text + " is a Palindrome!");
+} else {
+   Console.WriteLine (" " + text + " is not a Palindrome!");
+}
 
-   
+      
+
