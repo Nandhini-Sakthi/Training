e9d8d7258fc5680cc1a2be43f3d18c9501d665a2
nandhini <nandhini.s.interns@metamation.com>
2023-08-14 14:44:18 +0530

    changes done

--- Program.cs
+++ Program.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 for (; ; ) {
    Console.Write ("Enter a decimal number: ");
-   if (int.TryParse (Console.ReadLine (), out int decimalnum) && decimalnum>0) {
-      Console.WriteLine ($"Hexadecimal:{decimalnum:X}");
-      string binary = "";
-      while (decimalnum > 0) {
-         int remainder = decimalnum % 2;
-         binary = remainder.ToString () + binary;
-         decimalnum /= 2;
+   if (int.TryParse (Console.ReadLine (), out int decimalnum)) {
+      if (decimalnum >= 0) {
+         Console.WriteLine ($"Hexadecimal: {decimalnum:X}");
+         string binary = "";
+         if (decimalnum == 0) Console.WriteLine ($"Binary : 00000000");
+         else {
+            while (decimalnum > 0) {
+               int remainder = decimalnum % 2;
+               binary = remainder.ToString () + binary;
+               decimalnum /= 2;
+            }
+            Console.WriteLine ($"Binary:{binary}");
+         }
       }
-      Console.WriteLine ($"Binary:{binary}");
-   }
-   else
-      Console.WriteLine ("enter valid number");
+   }else Console.WriteLine ("enter valid number");
 }
+   
