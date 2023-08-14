ef209bce4d48d059f3d98948f5e1bf5bcce8b468
nandhini <nandhini.s.interns@metamation.com>
2023-08-14 15:21:12 +0530

    changes done

--- Program.cs
+++ Program.cs
@@ -1,15 +1,23 @@
 ﻿using System;
-int firstNum = 0, SecondNum = 1, nextNum, num;
-Console.Write ("Enter the number: ");
-num = int.Parse (Console.ReadLine ());
-if (num < 2) {
-   Console.Write ("Please Enter a number greater than two");
-} else {
-   Console.Write (firstNum + " " + SecondNum + " ");
-   for (int i = 2; i < num; i++) {
-      nextNum = firstNum + SecondNum;
-      Console.Write (nextNum + " ");
-      firstNum = SecondNum;
-      SecondNum = nextNum;
-   }
+for (; ; ) {
+   Console.Write ("Enter the number: ");
+   int firstNum = 0, SecondNum = 1, nextNum, num;
+   if (int.TryParse (Console.ReadLine (), out num)) {
+      if (num < 2) {
+         Console.Write ("Please Enter a number greater than two");
+      } else {
+         Console.Write (firstNum + " " + SecondNum + " ");
+         for (int i = 2; i < num; i++) {
+            nextNum = firstNum + SecondNum;
+            Console.Write (nextNum + " ");
+            firstNum = SecondNum;
+            SecondNum = nextNum;
+         }
+      }
+   } else Console.WriteLine ("Enter Valid number");
 }
+
+
+
+
+
