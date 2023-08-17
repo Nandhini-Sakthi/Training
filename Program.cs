ebd6b20b312a7be63679a7c87890d3d61dbc4692
nandhini <nandhini.s.interns@metamation.com>
2023-08-17 10:02:52 +0530

    basic assignment T08

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
-Console.WriteLine ("Hello, World!");
-
+for(; ; ) { 
+Console.Write("Enter the number:");
+if (int.TryParse (Console.ReadLine (), out int n)) {
+   for (int i = 1; i <= 10; i++)
+      Console.Write ("{0} X {1,2}  = {2} \n", n, i, n * i);
+} else Console.WriteLine ("Enter valid number");
+}
    
