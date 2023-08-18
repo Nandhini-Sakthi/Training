7f5e83251a5fcd5f8bf83478e45c2875201339a4
nandhini <nandhini.s.interns@metamation.com>
2023-08-18 16:49:45 +0530

    changes done

--- Program.cs
+++ Program.cs
@@ -4,17 +4,16 @@ for (; ; ) {
    int.TryParse (Console.ReadLine (), out int a);
    Console.Write ("Enter the second number: ");
    int.TryParse (Console.ReadLine (), out int b);
-   int newA = a;
    int gcd = CalculateGCD (a, b);
-   int lcm = (newA * b) / gcd;
-   Console.WriteLine ($"GCD of {newA} and {b} is: {gcd}");
-   Console.WriteLine ($"LCM of {newA} and {b} is: {lcm}");
-   static int CalculateGCD (int a, int b) {
-      while (b != 0) {
-         int temp = b;
-         b = a % b;
-         a = temp;
-      }
-      return a;
+   int lcm = (a * b) / gcd;
+   Console.WriteLine ($"GCD of {a} and {b} is: {gcd}");
+   Console.WriteLine ($"LCM of {a} and {b} is: {lcm}");
+}
+int CalculateGCD (int X, int Y) {
+   while (Y != 0) {
+      int temp = Y;
+      Y = X % Y;
+      X = temp;
    }
+   return X;
 }
