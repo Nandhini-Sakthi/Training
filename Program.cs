6d3400cebcc9d03ef62e3efb9c287b42050f83d6
nandhini <nandhini.s.interns@metamation.com>
2023-08-21 11:02:22 +0530

    basic assignment T11

--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
 ﻿using System;
-Console.WriteLine ("Hello, World!");
-
-   
+for ( ; ; ) {
+   Console.Write ("Enter the number:");
+   if (int.TryParse (Console.ReadLine (), out int num)) {
+      while (num >= 10) {
+         int sum = 0;
+         while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+         }
+         num = sum;
+      } Console.WriteLine (num);
+   } else Console.WriteLine ("Enter valid number");
+}
