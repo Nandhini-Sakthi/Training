9c6742bd1e61325505574c3daf60a1c4b205773e
nandhini <nandhini.s.interns@metamation.com>
2023-08-21 09:45:56 +0530

    basic assignment T10

--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
 ﻿using System;
-Console.WriteLine ("Hello, World!");
-
-   
+for (; ; ) {
+   Console.Write ("Enter the number:");
+   int.TryParse (Console.ReadLine (), out int number);
+   int orgNumber = number;
+   int revNumber = 0;
+   while (number > 0) {
+      int lastDigit = number % 10;
+      revNumber = revNumber * 10 + lastDigit;
+      number /= 10;
+   }
+   if (revNumber == orgNumber)
+      Console.WriteLine ("It is a Palindrome");
+   else Console.WriteLine ("It is not a Palindrome");
+}
