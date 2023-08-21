791a8e912ac646e470c4385d9267547b9d44c02b
nandhini <nandhini.s.interns@metamation.com>
2023-08-21 17:00:29 +0530

    basic assignment T12

--- Program.cs
+++ Program.cs
@@ -1,4 +1,17 @@
 ﻿using System;
-Console.WriteLine ("Hello, World!");
-
-   
+for (; ; ) {
+   Console.Write ("Enter the rows:");
+   if (int.TryParse (Console.ReadLine (), out int rows)) {
+      for (int i = 0; i < rows; i++) {
+         for (int j = 0; j < rows - i; j++) {
+            Console.Write (" ");
+         }
+         int val = 1;
+         for (int j = 0; j <= i; j++) {
+            Console.Write (val + " ");
+            val = val * (i - j) / (j + 1);
+         }
+         Console.WriteLine ();
+      }
+   }else Console.WriteLine ("Enter valid number");
+}
