c2544c5c62fdcb11414d6b4b277e1b7167fdcfa2
nandhini <nandhini.s.interns@metamation.com>
2023-08-28 15:48:39 +0530

    basic assignment T16

--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
 ﻿using System;
-Console.WriteLine ("Hello, World!");
-
-   
+using System.Linq;
+for (; ; ) {
+   Console.Write ("Enter the word : ");
+   string word = Console.ReadLine ().ToLower ();
+   char[] input = word.ToArray ();
+   Array.Sort (input);
+   string sortedInput = "";
+   foreach (char c in input) {
+      sortedInput += c;
+   }
+   string result = (word == sortedInput) ? "It is an Abecedarian word." : "It is NOT an Abecedarian word.";
+   Console.WriteLine (result);
+   Console.WriteLine ("Longest Abecedarian word : " + sortedInput);
+}
