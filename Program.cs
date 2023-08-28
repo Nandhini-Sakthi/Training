2de740a365a9a519fa8e33b58d3a521c05b7a6e3
nandhini <nandhini.s.interns@metamation.com>
2023-08-28 13:06:11 +0530

    basic assignment

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
-Console.WriteLine ("Hello, World!");
-
-   
+using System.Linq;
+for (; ; ) {
+   Console.Write ("Enter the word:");
+   string word = Console.ReadLine ().ToLower ();
+   if (word.Distinct ().Count () == word.Length)
+      Console.WriteLine ("True." + word + " is an isogram");
+   else Console.WriteLine ("False." + word + "is not an isogram");
+}
