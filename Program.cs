a8f3debd708f41eedc462e55539e285e9b3ae89f
nandhini <nandhini.s.interns@metamation.com>
2023-08-29 10:46:27 +0530

    basic assignment T18

--- Program.cs
+++ Program.cs
@@ -1,4 +1,19 @@
 ﻿using System;
-Console.WriteLine ("Hello, World!");
-
-   
+static double ArmstrongNumber (int num) {
+   string numStr = num.ToString ();
+   int n = numStr.Length;
+   double result = 0;
+   while (num != 0) {
+      int remainder = num % 10;
+      double power = Math.Pow (remainder, n);
+      result += power;
+      num /= 10;
+   }return result;
+}
+for (; ; ) {
+   Console.Write ("Enter an integer: ");
+   if (int.TryParse (Console.ReadLine (), out int num)) {
+      if (ArmstrongNumber (num) == num) Console.WriteLine (num + " is an Armstrong number.");
+      else Console.WriteLine (num + " is not an Armstrong number.");
+   } else Console.WriteLine ("Invalid input.");
+}
