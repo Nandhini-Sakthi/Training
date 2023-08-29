b7c5da3d6ab8f58ddeecb4ef43be2a29d374ef9f
nandhini <nandhini.s.interns@metamation.com>
2023-08-29 12:36:06 +0530

    basic assignment t1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
 ﻿using System;
-Console.WriteLine ("Hello, World!");
+for (; ; ) {
+   Console.Write ("Enter the number : ");
+   if (int.TryParse (Console.ReadLine (), out int num))
+      Console.WriteLine ("Factorial number for " + num + "! is " + FactorialNumber (num));
+   else Console.WriteLine ("Invalid Input");
+}
 
-   
+static int FactorialNumber (int num) {
+   int factorial = 1;
+   for (int i = 1; i <= num; i++) {
+      factorial = factorial * i;
+   }
+   return factorial;
+}
