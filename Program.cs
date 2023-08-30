f246a6edac837eca692356056021b320ca334ff3
nandhini <nandhini.s.interns@metamation.com>
2023-08-30 10:23:18 +0530

    basic assignment T21

--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 for (; ; ) {
-   Console.Write ("Enter the number :");
-   string input = Console.ReadLine ();
-   string[] substrings = input.Split ('.');
-   if (substrings.Length >= 2) {
-      string integralPart = substrings[0];
-      string factorialPart = substrings[1];
-      Console.WriteLine ("Integral part: " + integralPart + " Factorial part: " + factorialPart);
-   }
+   Console.Write ("Enter the number 1 :");
+   int.TryParse (Console.ReadLine (), out int num1);
+   Console.Write ("Enter the number 2 :");
+   int.TryParse (Console.ReadLine (), out int num2);
+   SwapNumber (ref num1, ref num2);
+   Console.WriteLine ($"After swapping :num1={num1},num2={num2}");
 }
+static void SwapNumber (ref int num1, ref int num2) {
+   int temp = num1;
+   num1 = num2;
+   num2 = temp;
 }
