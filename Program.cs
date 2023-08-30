9bb3075ed85c7aafd8d4af3ff2f6d3ca00508f77
nandhini <nandhini.s.interns@metamation.com>
2023-08-30 10:29:55 +0530

    basic assignment T20

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
-Console.WriteLine ("Hello, World!");
-
-   
+Console.Write ("Enter the number:");
+string input = Console.ReadLine ();
+string[] substrings = input.Split ('.');
+if (substrings.Length >= 2) { 
+   string integralPart = substrings[0];
+   string factorialPart = substrings[1];
+   Console.WriteLine ("Integral part: " + integralPart + " Factorial part: " + factorialPart);
+}
