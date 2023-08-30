eb863c13ffcd6fe5176390d8c96530612838d395
nandhini <nandhini.s.interns@metamation.com>
2023-08-30 12:14:16 +0530

    changes done

--- Program.cs
+++ Program.cs
@@ -1,17 +1,14 @@
 ﻿using System;
 Console.Write ("Enter a word or phrase:");
 string text = Console.ReadLine ();
-text = text.Replace (" ", "").ToLower ();
-if (text.Equals (ReverseString (text))) Console.WriteLine (text + " is a Palindrome!");
+string original = text.Replace (" ", "").ToLower ();
+if (original.Equals (ReverseString (original))) Console.WriteLine (text + " is a Palindrome!");
 else Console.WriteLine (text + " is not a Palindrome!");
 
-string ReverseString (string text) {
-   char[] chars = text.ToCharArray ();
-   char[] reverse = new char[chars.Length];
-   int j = chars.Length - 1;
-   for (int i = 0; i < chars.Length; i++) {
-      reverse[i] = chars[j];
-      j--;
-   }
-   return new string (reverse);
+string ReverseString (string original) {
+   char[] chars = original.ToCharArray ();
+   string rev = "";
+   for (int i = chars.Length - 1; i >= 0; i--)
+      rev += chars[i];
+   return rev;
 }
