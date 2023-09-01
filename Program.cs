a350a3ddf6f427aff5ed7e8c9138812d05dc9536
nandhini <nandhini.s.interns@metamation.com>
2023-09-01 14:23:58 +0530

    basic assignment T18.1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,30 @@
 ﻿using System;
-Console.WriteLine ("Hello, World!");
-
-   
+if (int.TryParse (args[0], out int num)) 
+   Console.WriteLine ($"The {num}th Armstrong number is: " + NthArmstrongNumber (num));
+else {
+   Console.WriteLine ("Please provide a positive integer as input.");
+   return;
+}
+static int NthArmstrongNumber (int num) {
+   int count = 0;
+   int i = 0;
+   while (count < num) {
+      i++;
+      if (IsArmstrong (i)) {
+         count++;
+      }
+   }
+    return i;
+}
+static bool IsArmstrong (int i) {
+   int n = i.ToString ().Length;
+   int temp = i;
+   int result = 0;
+   while (temp != 0) {
+      int remainder = temp % 10;
+      int power = (int) Math.Pow (remainder, n);
+      result += power;
+      temp /= 10;
+   }
+   return result == i;
+}
