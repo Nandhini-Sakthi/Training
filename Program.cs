8748b98a4bf8b36006553b270976926949c93f01
nandhini <nandhini.s.interns@metamation.com>
2023-09-01 17:14:44 +0530

    basic assignment T22

--- Program.cs
+++ Program.cs
@@ -1,4 +1,27 @@
 ﻿using System;
-Console.WriteLine ("Hello, World!");
 
-   
+class Program {
+   static void Main () {
+      Random random = new Random ();
+      int[] numbers = new int[10];
+      int i;
+
+      for ( i = 0; i < numbers.Length; i++)
+         numbers[i] = random.Next (1, 10);
+
+      Console.WriteLine ("Original: " + string.Join (" ", numbers));
+
+      Console.Write ("Enter two indices to swap (e.g., 2 3): ");
+      string[] input = Console.ReadLine ().Split ();
+
+      if (input.Length == 2 && int.TryParse (input[0], out  i) && int.TryParse (input[1], out int j) && i >= 0 && i < numbers.Length && j >= 0 && j < numbers.Length) {
+         int temp = numbers[i];
+         numbers[i] = numbers[j];
+         numbers[j] = temp;
+
+         Console.WriteLine ("Swapped: " + string.Join (" ", numbers));
+      } else {
+         Console.WriteLine ("Invalid input or indices.");
+      }
+   }
+}
