0be244ae61ba5ca028e9698d8fa53e49aae7c413
nandhini <nandhini.s.interns@metamation.com>
2023-09-04 17:01:12 +0530

    changes done

--- Program.cs
+++ Program.cs
@@ -1,28 +1,21 @@
 ﻿using System;
-if (int.TryParse (args[0], out int num)) 
-   Console.WriteLine ($"The {num}th Armstrong number is: " + NthArmstrongNumber (num));
-else {
-   Console.WriteLine ("Please provide a positive integer as input.");
-   return;
-}
+if (int.TryParse (args[0], out int num)) Console.WriteLine ($"The {num}th Armstrong number is: " + NthArmstrongNumber (num));
+else  Console.WriteLine ("Invalid Input");
 static int NthArmstrongNumber (int num) {
-   int count = 0;
-   int i = 0;
-   while (count < num) {
-      i++;
-      if (IsArmstrong (i)) {
-         count++;
-      }
+   int count = 0, i = 0;
+   if (num < 10) return num;
+   else {
+      while (count < num)
+         if (IsArmstrong (++i)) count++;
    }
-    return i;
+   return i;
 }
 static bool IsArmstrong (int i) {
    int n = i.ToString ().Length;
-   int temp = i;
-   int result = 0;
+   int temp = i, result = 0;
    while (temp != 0) {
       int remainder = temp % 10;
-      int power = (int) Math.Pow (remainder, n);
+      int power = (int)Math.Pow (remainder, n);
       result += power;
       temp /= 10;
    }
