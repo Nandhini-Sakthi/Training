6df4efb4d8724d1e62fd7a38f6914069416bc268
nandhini <nandhini.s.interns@metamation.com>
2023-09-07 14:24:18 +0530

    changes done

--- Program.cs
+++ Program.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 for (; ; ) {
    Console.Write ("Enter the number:");
-   int.TryParse (Console.ReadLine (), out int number);
-   int orgNumber = number;
-   int revNumber = 0;
-   while (number > 0) {
-      int lastDigit = number % 10;
-      revNumber = revNumber * 10 + lastDigit;
-      number /= 10;
+   if (int.TryParse (Console.ReadLine (), out int number)) {
+      int orgNumber = number;
+      int revNumber = 0;
+      while (number > 0) {
+         int lastDigit = number % 10;
+         revNumber = revNumber * 10 + lastDigit;
+         number /= 10;
+      }
+      if (revNumber == orgNumber)
+         Console.WriteLine ("It is a Palindrome");
+      else Console.WriteLine ("It is not a Palindrome");
    }
-   if (revNumber == orgNumber)
-      Console.WriteLine ("It is a Palindrome");
-   else Console.WriteLine ("It is not a Palindrome");
+   else Console.WriteLine ("Please enter valid input.");
 }
