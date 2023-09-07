781c07e3f6c594fb67a7578c562ba29b99085b55
nandhini <nandhini.s.interns@metamation.com>
2023-09-07 14:50:38 +0530

    changes done

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 Console.WriteLine ("Multiplication Table ");
 for (int i = 1; i <= 10; i++) {
-   Console.WriteLine ($"Table for {i}:");
-   for (int j = 1; j <= 10; j++) Console.WriteLine ("{0} X {1,2} = {2} ", i, j, i * j);
+   Console.WriteLine ($"\nTable for {i}:");
+   for (int j = 1; j <= 10; j++) Console.WriteLine ("{0} * {1,2} = {2} ", i, j, i * j);
 }
