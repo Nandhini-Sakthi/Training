2477e3688c214414e3b52db23725c315f3364610
nandhini <nandhini.s.interns@metamation.com>
2023-09-11 17:03:46 +0530

    changes done

--- Program.cs
+++ Program.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 for (; ; ) {
    Console.Write ("Enter the number : ");
-   if (int.TryParse (Console.ReadLine (), out int num))
-      Console.WriteLine ("Factorial number for " + num + "! is " + FactorialNumber (num));
+   if (int.TryParse (Console.ReadLine (), out int num)) Console.WriteLine ("Factorial number for " + num + "! is " + FactorialNumber (num));
    else Console.WriteLine ("Invalid Input");
 }
 
-static int FactorialNumber (int num) {
-   int factorial = 1;
-   for (int i = 1; i <= num; i++) {
-      factorial = factorial * i;
-   }
+static long FactorialNumber (int num) {
+   long factorial = 1;
+   for (int i = 1; i <= num; i++) factorial *= i;
    return factorial;
 }
