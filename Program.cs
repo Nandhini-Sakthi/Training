412bce1801b92011199018c56499dac169fd7f94
nandhini <nandhini.s.interns@metamation.com>
2023-09-11 16:52:18 +0530

    changes done

--- Program.cs
+++ Program.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Linq;
-for (; ; ) {
-   Console.Write ("Enter the word : ");
-   string word = Console.ReadLine ().ToLower ();
-   char[] input = word.ToArray ();
-   Array.Sort (input);
-   string sortedInput = "";
-   foreach (char c in input) {
-      sortedInput += c;
-   }
-   string result = (word == sortedInput) ? "It is an Abecedarian word." : "It is NOT an Abecedarian word.";
-   Console.WriteLine (result);
-   Console.WriteLine ("Longest Abecedarian word : " + sortedInput);
+string[] words = { "apple", "effort", "cherry", "glory", "fig", "grape", "aegilops", "adopt", "mother", "office" };
+string longestAbecedarianWord = FindLongestAbecedarianWord (words);
+var res = string.IsNullOrEmpty (longestAbecedarianWord) ? $"No abecedarian word found in the array." : $"Longest Abecedarian Word:{longestAbecedarianWord}";
+Console.WriteLine (res);   
+
+bool IsAbecedarian (string word) {
+   string sortedWord = new (word.OrderBy (c => c).ToArray ());
+   return word == sortedWord;
+}
+
+string FindLongestAbecedarianWord (string[] words) {
+   string longestAbecedarianWord = "";
+   foreach (string word in words) if (IsAbecedarian (word) && word.Length > longestAbecedarianWord.Length) longestAbecedarianWord = word;
+   return longestAbecedarianWord;
 }
