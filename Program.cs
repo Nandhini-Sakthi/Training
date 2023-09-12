38b0d21a6b6c7712a9e2d0be144547ce1f03b469
nandhini <nandhini.s.interns@metamation.com>
2023-09-12 09:58:25 +0530

    changes done

--- Program.cs
+++ Program.cs
@@ -3,12 +3,9 @@ using System.Linq;
 string[] words = { "apple", "effort", "cherry", "glory", "fig", "grape", "aegilops", "adopt", "mother", "office" };
 string longestAbecedarianWord = FindLongestAbecedarianWord (words);
 var res = string.IsNullOrEmpty (longestAbecedarianWord) ? $"No abecedarian word found in the array." : $"Longest Abecedarian Word:{longestAbecedarianWord}";
-Console.WriteLine (res);   
+Console.WriteLine (res);
 
-bool IsAbecedarian (string word) {
-   string sortedWord = new (word.OrderBy (c => c).ToArray ());
-   return word == sortedWord;
-}
+bool IsAbecedarian (string word) => word == new string (word.OrderBy (c => c).ToArray ());
 
 string FindLongestAbecedarianWord (string[] words) {
    string longestAbecedarianWord = "";
