f2b0e674837c2910f4eeaaef64be094c959c3a73
nandhini <nandhini.s.interns@metamation.com>
2023-09-12 09:51:48 +0530

    changes done

--- Program.cs
+++ Program.cs
@@ -3,8 +3,8 @@ using System.Linq;
 for (; ; ) {
    Console.Write ("Enter the word:");
    string word = Console.ReadLine ().ToLower ();
-   var res = (!Isogram (word)) ? word + " is not an isogram." : word + " is an isogram.";
-   Console.WriteLine (res);
+   var res = (Isogram (word)) ? "is an isogram." : "is not an isogram.";
+   Console.WriteLine ($"{word} {res}");
 }
 
 bool Isogram (string word) => word.Distinct ().Count () == word.Length;
