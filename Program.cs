04ef9724d77a725ec8b88dc4a9dad84152c791e1
nandhini <nandhini.s.interns@metamation.com>
2023-09-13 10:25:33 +0530

    changes done

--- Program.cs
+++ Program.cs
@@ -2,12 +2,13 @@
 for (; ; ) {
    Console.Write ("Enter an integer: ");
    if (int.TryParse (Console.ReadLine (), out int num)) {
-      if (ArmstrongNumber (num) == num) Console.WriteLine (num + " is an Armstrong number.");
+      if (IsArmstrongNumber (num)) Console.WriteLine (num + " is an Armstrong number.");
       else Console.WriteLine (num + " is not an Armstrong number.");
    } else Console.WriteLine ("Invalid input.");
 }
 
-static double ArmstrongNumber (int num) {
+bool IsArmstrongNumber (int num) {
+   int input = num;
    string numStr = num.ToString ();
    int n = numStr.Length;
    double result = 0;
@@ -17,5 +18,5 @@ static double ArmstrongNumber (int num) {
       result += power;
       num /= 10;
    }
-   return result
+   return result == input;
 }
