43bfe6aeb34ca430fa4ccffdf569875c71e47f6b
nandhini <nandhini.s.interns@metamation.com>
2023-09-20 10:56:10 +0530

    changes done

--- Program.cs
+++ Program.cs
@@ -1,32 +1,26 @@
 ﻿string[] wordslist = File.ReadAllLines ("D:\\OneDrive - Trumpf Metamation Pvt Ltd\\words.txt");
 char[] letters = { 'U', 'X', 'A', 'L', 'T', 'N', 'E' };
-string[] words = new string[wordslist.Length];
-int[] pointArr = new int[wordslist.Length];
-int point = 0, index = 0, total = 0;
+Dictionary<string, int> wordPoints = new Dictionary<string, int> ();
+int total=0,point = 0;
 
 foreach (string word in wordslist) {
    if (word.Length >= 4 && word.Contains ('U') && word.All (letters.Contains)) {
-      if (letters.All (letter => word.Contains (letter))) {
+      if (letters.All (letter => word.Contains (letter)))
          point = word.Length + 7;
-         //Console.WriteLine ($"{word}    {point}");
-      } else
+      else
          point = word.Length == 4 ? 1 : word.Length;
-      // Console.WriteLine ($"{word}    {point}");
-      words[index] = word;
-      pointArr[index] = point;
-      index++;
-      total = total + point;
+      wordPoints[word] = point;
+      total += point;
    }
 }
-Array.Sort (pointArr, words,0, index, Comparer<int>.Create ((a, b) => b.CompareTo (a)));
-for (int i = 0; i < index; i++) {
-   if (pointArr[i] == 15) {
+
+var sortedWordPoints = wordPoints.OrderByDescending (kv => kv.Value).ToList ();
+
+foreach (var kvp in sortedWordPoints) {
+   if (kvp.Value == 15) 
       Console.ForegroundColor = ConsoleColor.Green;
-
-   }
-
-   Console.WriteLine ($"{pointArr[i]}. {words[i]}");
+   Console.WriteLine ($"{kvp.Value,2}. {kvp.Key}");
    Console.ResetColor ();
 }
 Console.WriteLine ("----");
-Console.WriteLine ($"{total} totat points");
+Console.WriteLine ($"{total} total");
