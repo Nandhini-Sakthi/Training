9fe46805b9aab766bc07ff8dbb4bdbd0a05fda53
nandhini <nandhini.s.interns@metamation.com>
2023-10-03 16:55:39 +0530

    T23

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿// --------------------------------------------------------------------------------------------
 // Training ~ A training program for new interns at Metamation, Batch - July 2023
-// Copyright (c) Metamation India.                                              
+// Copyright (c) Metamation India.
 // ------------------------------------------------------------------------
-// Program.cs
+// VOTING CONTEST 
 // Given a string S to a method, with each character in it representing a vote for a contestant, return the winner with the most votes.
 // For example,input=Helloworld;output wil be L, 3 => Winner and votes.
 // --------------------------------------------------------------------------------------------
@@ -12,38 +12,38 @@ using System.Collections.Generic;
 
 namespace Training {
    #region Program ------------------------------------------------------------------------------
-   /// <summary>Sample program</summary>
+   /// <summary>Voting Contest</summary>
    internal class Program {
       #region Methods ---------------------------------------------
       /// <summary>getting input from the user</summary>
       static void Main () {
          Console.WriteLine ("Enter the string: ");
-         string input = Console.ReadLine ();
+         string input = Console.ReadLine ()!;
          FindWinner (input, out char winner, out int votes);
          Console.WriteLine ($"{winner}, {votes} => Winner and votes");
-        // find winner based on votes
-         static void FindWinner (string input, out char winner, out int votes) {
-            Dictionary<char, int> voteCount = new Dictionary<char, int> ();//store character and votes.
-            foreach (char c in input.ToLower ()) {
-               if (Char.IsLetter (c)) {
-                  if (voteCount.ContainsKey (c)) {
-                     voteCount[c]++;
-                  } else {
-                     voteCount[c] = 1;
-                  }
-               }
+      }
+      /// <summary>find the winner based on votes</summary>
+      /// <param name="input">word</param>
+      /// <param name="winner">letter with maximum votes</param>
+      /// <param name="votes">votes</param>
+      static void FindWinner (string input, out char winner, out int votes) {
+         Dictionary<char, int> voteCount = new Dictionary<char, int> ();//store character and votes.
+         foreach (char c in input.ToLower ()) {
+            if (Char.IsLetter (c)) {
+               if (voteCount.ContainsKey (c)) voteCount[c]++;
+               else voteCount[c] = 1;
             }
-            char currentWinner = ' ';
-            int maxVotes = 0;
-            foreach (var kvp in voteCount) {
-               if (kvp.Value >= maxVotes || (kvp.Value == maxVotes && kvp.Key < currentWinner)) {//find which char has the most votes.
-                  currentWinner = kvp.Key;
-                  maxVotes = kvp.Value;
-               }
-            }
-            winner = currentWinner;
-            votes = maxVotes;
          }
+         char currentWinner = ' ';
+         int maxVotes = 0;
+         foreach (var kvp in voteCount) {
+            if (kvp.Value >= maxVotes || (kvp.Value == maxVotes && kvp.Key < currentWinner)) {//find which char has the most votes.
+               currentWinner = kvp.Key;
+               maxVotes = kvp.Value;
+            }
+         }
+         winner = currentWinner;
+         votes = maxVotes;
       }
       #endregion
    }
