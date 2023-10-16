b20caf6013297ad0f16d8d1ff36b705fe133502e
nandhini <nandhini.s.interns@metamation.com>
2023-10-16 11:06:49 +0530

    made some changes

--- Program.cs
+++ Program.cs
@@ -13,15 +13,14 @@ using System.Linq;
 // Reading all letter in the wordlist.
 string wordsList = File.ReadAllText ("D:\\OneDrive - Trumpf Metamation Pvt Ltd\\words.txt");
 Dictionary<char, int> output = new ();
-
 foreach (char ch in wordsList) {
    if (ch >= 'A' && ch <= 'Z') {
       // Initialize count to 1 for a new character
       if (!output.ContainsKey (ch)) output[ch] = 1;
       // Increment count for an existing character
-      else output[ch]++; 
+      else output[ch]++;
    }
 }
 // Arrange the letters in descending order based on the value.
-foreach (var kvp in output.OrderByDescending (kv => kv.Value).Take (7)) 
+foreach (var kvp in output.OrderByDescending (kv => kv.Value).Take (7))
    Console.WriteLine ($"Character: {kvp.Key} , Count: {kvp.Value}");
