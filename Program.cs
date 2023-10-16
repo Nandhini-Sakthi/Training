8aabf66efed0fb38a5edbfd9aac91ef3ce328fcf
nandhini <nandhini.s.interns@metamation.com>
2023-10-16 16:13:49 +0530

    formatting the code is done

--- Program.cs
+++ Program.cs
@@ -31,25 +31,29 @@ list.RemoveAt (0);
 list.RemoveAt (1);
 list.Display ();
 
-/// <summary> Create a class  </summary>
-/// <typeparam name="T"> Datatype of the list </typeparam>
+/// <summary>Create a class</summary>
+/// <typeparam name="T">Datatype of the list</typeparam>
 public class MyList<T> {
    private T[] mArray;
    private int mCount;
-   /// <summary> Creating a Constructor for initializing the values </summary>
+   /// <summary>Creating a Constructor for initializing the values</summary>
+
    public MyList () {
       // Initialize the array capacity as 4. 
       mArray = new T[4];
       mCount = 0;
    }
-   /// <summary> Counting the elememt in the list </summary>
+   /// <summary>Counting the elememt in the list</summary>
+
    public int Count => mCount;
-   /// <summary> Calculating capacity of the array </summary>
+   /// <summary>Calculating capacity of the array</summary>
+
    public int Capacity => mArray.Length;
-   /// <summary> Set and return the value based on the index </summary>
-   /// <param name="index"> Index to be change </param>
-   /// <returns> Changed value of the index  </returns>
-   /// <exception cref="IndexOutOfRangeException"> When the index is out of range </exception>
+   /// <summary>Set and return the value based on the index</summary>
+   /// <param name="index">Index to be change</param>
+   /// <returns>Changed value of the index</returns>
+   /// <exception cref="IndexOutOfRangeException">When the index is out of range</exception>
+
    public T this[int index] {
       get {
          if (index < 0 || index >= mCount) throw new IndexOutOfRangeException ("Index is out of the valid range.");
@@ -60,17 +64,19 @@ public class MyList<T> {
          mArray[index] = value;
       }
    }
-   /// <summary> Add the value to the end </summary>
-   /// <param name="a"> Value to be add </param>
+   /// <summary>Add the value to the end</summary>
+   /// <param name="a">Value to be add</param>
+
    public void Add (T a) {
       if (mCount == mArray.Length) Array.Resize (ref mArray, mArray.Length * 2); // Double the capacity when needed
       mArray[mCount] = a;
       mCount++;
    }
-   /// <summary> Remove the value </summary>
-   /// <param name="a"> Value to be remove </param>
-   /// <returns> True </returns>
-   /// <exception cref="InvalidOperationException"> When the index is out of range </exception>
+   /// <summary>Remove the value</summary>
+   /// <param name="a">Value to be remove</param>
+   /// <returns>True</returns>
+   /// <exception cref="InvalidOperationException">When the index is out of range</exception>
+
    public bool Remove (T a) {
       int index = Array.IndexOf (mArray, a);
       if (index < 0)
@@ -80,15 +86,17 @@ public class MyList<T> {
       mCount--;
       return true;
    }
-   /// <summary> Clear the elements </summary>
+   /// <summary>Clear the elements</summary>
+
    public void Clear () {
       Array.Clear (mArray, 0, mCount);
       mCount = 0;
    }
-   /// <summary> Insert the value  </summary>
-   /// <param name="index"> The index where to add </param>
-   /// <param name="item"> The value to be add </param>
-   /// <exception cref="IndexOutOfRangeException"> When the index is not in range </exception>
+   /// <summary>Insert the value</summary>
+   /// <param name="index">The index where to add</param>
+   /// <param name="item">The value to be add</param>
+   /// <exception cref="IndexOutOfRangeException">When the index is not in range</exception>
+
    public void Insert (int index, T item) {
       if (index < 0 || index > mCount)
          throw new IndexOutOfRangeException ("Index is out of the valid range.");
@@ -99,9 +107,10 @@ public class MyList<T> {
       mArray[index] = item;
       mCount++;
    }
-   /// <summary> Remove the value </summary>
-   /// <param name="index"> Index where to remove the element </param>
-   /// <exception cref="IndexOutOfRangeException"> When the index is not in range </exception>
+   /// <summary>Remove the value</summary>
+   /// <param name="index">Index where to remove the element</param>
+   /// <exception cref="IndexOutOfRangeException">When the index is not in range</exception>
+
    public void RemoveAt (int index) {
       if (index < 0 || index >= mCount)
          throw new IndexOutOfRangeException ("Index is out of the valid range.");
@@ -109,7 +118,8 @@ public class MyList<T> {
          mArray[i] = mArray[i + 1];
       mCount--;
    }
-   /// <summary> Display the list </summary>
+   /// <summary>Display the list</summary>
+
    public void Display () {
       for (int i = 0; i < mCount; i++)
          Console.Write (mArray[i]);
