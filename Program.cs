985eed1153c2219e9d84d95d5dfae8e93e865ef9
nandhini <nandhini.s.interns@metamation.com>
2024-01-24 18:05:48 +0530

    Implemented

--- Program.cs
+++ Program.cs
@@ -1,54 +1,54 @@
-﻿//Complex Number
+﻿//Complex number.
 using System;
 
-namespace Test {
-
-   internal class Program {
+namespace Training {
+   #region Class Program -----------------------------------------------------------------------
+   class Program {
+      #region Method ---------------------------------------------
+      //Gets input
       static void Main (string[] args) {
-         Console.WriteLine ("enter");
-         string input = Console.ReadLine ();
-         ComplexNumber complexNumber = new ComplexNumber ();
-         Double output = complexNumber.Norm (input);
-         Console.WriteLine (output);
-      }
-
-      #region class ComplexNumber --------------------------------------------------------------
-      /// <summary>Complex number</summary>
-      public class ComplexNumber {
-         #region method ------------------------------------------
-         /// <summary>Gets add and subtraction value</summary>
-         /// <param name="input">Complex number</param>
-         /// <returns>Complex number's magnitude value</returns>
-         public double Norm (string input) {
-            if (input.Contains ('+')) {
-               char op = '+';
-               return Calculate (op, input);
-            } else if (input.Contains ('-')) {
-               char op = '-';
-               return Calculate (op, input);
-            } else { return double.NaN; }
-         }
-
-         /// <summar>Calculate the values using operators</summary>
-         /// <param name="op">Operators</param>
-         /// <param name="input">Complex number</param>
-         /// <returns>Values for complex number</returns>
-         public Double Calculate (char op, string input) {
-            string[] a = input.Split (op);
-            double real = Double.Parse (a[0]);
-            string img = "";
-            foreach (char c in a[1]) if (c != 'i') img += c;
-            double imag = Double.Parse (img);
-            if (op == '+') {
-               double exp = (real * real) + (imag * imag);
-               return Math.Pow (exp, 0.5);
-            } else {
-               double exp = (real * real) - (imag * imag);
-               return Math.Pow (exp, 0.5);
-            }
-            #endregion
-         }
+         ComplexNumber complexNumber1 = new (4, 5);
+         ComplexNumber complexNumber2 = new (2, 3);
+         // Addition
+         Console.WriteLine ($"Sum: {ComplexNumber.Add (complexNumber1, complexNumber2)}");
+         // Subtraction
+         Console.WriteLine ($"Difference: {ComplexNumber.Sub (complexNumber1, complexNumber2)}");
+         // Norm
+         Console.WriteLine ($"Norm of complexNumber1: {complexNumber1.Norm}");
          #endregion
       }
+      #endregion
    }
+
+   #region Class ComplexNumber -----------------------------------------------------------------
+   /// <summary>Implementation of complex number</summary>
+   public class ComplexNumber {
+      #region Constructor ----------------------------------------
+      /// <summary>Members get initialize</summary>
+      /// <param name="real">Real part</param>
+      /// <param name="imaginary">Imaginary part</param>
+      public ComplexNumber (double real, double imaginary) {
+         Real = real;
+         Imaginary = imaginary;
+      }
+      #endregion
+
+      #region Property -------------------------------------------
+      public double Real { get; set; }
+      public double Imaginary { get; set; }
+      public double Norm { get => Math.Sqrt (Real * Real + Imaginary * Imaginary); }
+      #endregion
+
+      #region Method ---------------------------------------------
+      // Addition of two complex numbers
+      public static ComplexNumber Add (ComplexNumber a, ComplexNumber b) => new (a.Real + b.Real, a.Imaginary + b.Imaginary);
+
+      // Subtraction of two complex numbers
+      public static ComplexNumber Sub (ComplexNumber a, ComplexNumber b) => new (a.Real - b.Real, a.Imaginary - b.Imaginary);
+
+      // Overrides the default ToString method to the ComplexNumber as "Real + Imaginaryi" for conversion.
+      public override string ToString () =>$"{Real} + {Imaginary}i";
+      #endregion
+   }
+   #endregion
 }
